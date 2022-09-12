87e58730b2cfdfae344d0414c502a431d00239bb
dasha <dashasv76@gmail.com>
2022-09-12 14:38:01 +0300

    clone my-bike landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  form.reset();
+
+  window.location.href = '#header';
+});
+
+const footer = document.querySelector('.footer');
+
+footer.addEventListener('click', () => {
+  window.location.href = '#header';
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
